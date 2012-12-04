fdfe6fac0c83e72d6c9346dca3c1da6d86e62157
Kevin J. Lynagh <kevin@keminglabs.com>
2012-12-04 15:00:07 -0800

    Record constructors should accept an optional hash of keyword args.

--- lib/c2po/records.rb
+++ lib/c2po/records.rb
@@ -1,5 +1,8 @@
 module C2PO
   class C2PORecord < Hash
+    def initialize(h={})
+      self.merge! h
+    end
     def to_edn
       prefix, name = self.class.name.split("::").map(&:downcase)
       EDN.tagout "com.keminglabs/#{prefix}$#{name}", {}.merge(self)
